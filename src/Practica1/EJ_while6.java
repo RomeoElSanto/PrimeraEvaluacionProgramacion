1ffe61b4cecdd25a46eddabf5c2e896eadbafe0f
Emilio Gaete <emilio.gaete@educa.madrid.org>
2020-11-25 16:23:09 +0100

    commit trabajo programación

---
+++ src/Practica1/EJ_while6.java
@@ -0,0 +1,13 @@
+package Practica1;
+
+public class EJ_while6 {
+    public static void main(String[] args) {
+        int i=1;
+        int suma=0;
+        while (suma <= 125){
+            suma = suma + i;
+            i++;
+        }
+        System.out.println(i);
+    }
+}
